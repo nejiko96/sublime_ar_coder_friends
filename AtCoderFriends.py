d261abc912124ff4bdd0aa98efe57867ef3a1287
nejiko96 <nejiko2006@gmail.com>
2018-02-20 10:19:33 +0900

    rough implementation

--- AtCoderFriends.py
+++ AtCoderFriends.py
@@ -1,44 +1,50 @@
 import sublime
 import sublime_plugin
-from functools import partial
+
 import os
-import subprocess
 
 class AtCoderCommand(sublime_plugin.WindowCommand):
-  def get_file(self):
-    return self.window.active_view().file_name()
-
+  def run_on_file(self, af_cmd):
+    if self.window.active_view() and self.window.active_view().file_name():
+      path = self.window.active_view().file_name()
+    else:
+      sublime.error_message('AtCoderFriends: No file is opened')
+      return
+    args = ['at_coder_friends', af_cmd, path]
+    self.window.run_command('exec', {
+      'cmd': args,
+      'shell': True
+    })
 
 class AtCoderFriendsSetupCommand(sublime_plugin.WindowCommand):
   def run(self):
     if self.window.folders():
-      dir = self.window.folders()[0]
+      self.dir = self.window.folders()[0]
     else:
-      sublime.error_message('AtCoderFriends: No folder for the new contest')
-      return False
+      sublime.error_message('AtCoderFriends: No folder is opened')
+      return
     self.window.show_input_panel(
-      'Contest Name:', '', partial(self.on_done, dir), None, None
+      'Contest Name:', '', self.on_done, None, None
     )
 
-  def on_done(self, dir, contest):
-    if contest == "":
-      return False
-    path = os.path.join(dir, contest)
+  def on_done(self, contest):
+    if contest == '':
+      return
+    path = os.path.join(self.dir, contest)
     args = ['at_coder_friends', 'setup', path]
-    # print(args)
-    subprocess.call(args)
-
+    self.window.run_command('exec', {
+      'cmd': args,
+      'shell': True
+    })
 
 class AtCoderFriendsTestOneCommand(AtCoderCommand):
   def run(self):
-    print(self.get_file())
-
+    self.run_on_file('test-one')
 
 class AtCoderFriendsTestAllCommand(AtCoderCommand):
   def run(self):
-    print(self.paths[0])
-
+    self.run_on_file('test-all')
 
 class AtCoderFriendsSubmitCommand(AtCoderCommand):
-  def run(self, paths=[], parameters=None):
-    print(self.paths[0])
+  def run(self):
+    self.run_on_file('submit')
